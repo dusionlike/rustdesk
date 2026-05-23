ae214ba8d0eca5ec9075b69e63370131b4dd7665
zhangzhixiong <751069244@qq.com>
2026-05-23 23:00:43 +0800

    feat: no-ui版本支持windows

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
+    all(not(debug_assertions), target_os = "windows", not(feature = "no-ui")),
     windows_subsystem = "windows"
 )]
 
