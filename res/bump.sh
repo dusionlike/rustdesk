f8ef7e488ab2ecbcff967926caa19cbdbc22a3c8
zhangzhixiong <751069244@qq.com>
2026-08-16 18:51:44 +0800

    bump to 2.0.0

--- res/bump.sh
+++ res/bump.sh
@@ -1,3 +1,2 @@
 #! /usr/bin/env bash
-sed -i "s/\b$1\b/$2/g" res/*spec res/PKGBUILD flutter/pubspec.yaml Cargo.toml .github/workflows/*yml flatpak/*json appimage/*yml libs/portable/Cargo.toml
-cargo run # to bump version in cargo lock
+sed -i "s/\b$1\b/$2/g" res/*spec res/PKGBUILD flutter/pubspec.yaml Cargo.toml Cargo.lock .github/workflows/*yml flatpak/*json appimage/*yml libs/portable/Cargo.toml
--- res/rpm.spec
+++ res/rpm.spec
@@ -1,5 +1,5 @@
 Name:       rustdesk
-Version:    1.4.9
+Version:    2.0.0
 Release:    0
 Summary:    RPM package
 License:    GPL-3.0
